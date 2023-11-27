614e13fd151322e449ab03696ab4518392159f06
Louis Thurman <louisthurman@gmail.com>
2023-11-27 15:27:41 +0000

    Update day_template.py

--- day_template.py
+++ day_template.py
@@ -1,11 +1,16 @@
 from pathlib import Path
-from typing import Union
+from typing import Union, Iterator
 
 
 def raw_input(input_path: Path = Path("input.txt")) -> str:
     with open(input_path, "r") as fh:
         data = fh.read()
-    return data.strip()
+    return data
+
+
+def input_lines(input_path: Path = Path("input.txt")) -> Iterator[str]:
+    for line in raw_input(input_path).strip().split("\n"):
+        yield line.strip()
 
 
 def part_1() -> Union[int, str]:
@@ -18,4 +23,5 @@ def part_2() -> Union[int, str]:
 
 if __name__ == "__main__":
     print("Part 1:", part_1())
-    print("\nPart 2:", part_2())
+    print()
+    print("Part 2:", part_2())
