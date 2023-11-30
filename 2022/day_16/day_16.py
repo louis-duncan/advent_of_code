1d692b4a13bdeab3460ebc96ff7af710c7997b1c
Louis Thurman <louisthurman@gmail.com>
2023-11-30 13:50:26 +0000

    Retry 16 '22

--- 2022/day_16/day_16.py
+++ 2022/day_16/day_16.py
@@ -1,109 +1,76 @@
 import re
-from itertools import combinations
+from pathlib import Path
+from typing import Union, Iterator, Optional, Type, Any
 from math import inf
-from typing import List, Dict, Union, Set, FrozenSet
+
+"""
+https://adventofcode.com/2022/day/16
+"""
 
 
-def get_routes(connections: Dict[str, List[str]]):
-    routes: Dict[FrozenSet[str], int] = {}
+def raw_input(input_path: Path = Path("input.txt")) -> str:
+    with open(input_path, "r") as fh:
+        data = fh.read()
+    return data
 
-    target_num_routes = len(list(combinations(list(connections.keys()), 2)))
 
-    for c in connections:
-        for d in connections[c]:
-            routes[frozenset({c, d})] = 1
+def input_lines(input_path: Path = Path("input.txt"), convert_type: Optional[Type] = None) -> Iterator[Any]:
+    for line in raw_input(input_path).strip().split("\n"):
+        if convert_type is None:
+            yield line.strip()
+        else:
+            yield convert_type(line.strip())
 
-    for start in connections:
-        for end in connections:
-            if (frozenset({start, end}) in routes) or (start == end):
-                continue
 
-            unvisited_nodes = list(connections.keys())
-            distances: Dict[str, Union[int, float]] = {}
-            reached_from: Dict[str, str] = {}
-            for c in unvisited_nodes:
-                distances[c] = inf
-            distances[start] = 0
+def calc_total_release
 
-            while True:
-                subject_node = list(sorted(unvisited_nodes, key=lambda x: distances[x]))[0]
-                unvisited_nodes.remove(subject_node)
-                if subject_node == end:
+
+def part_1() -> Union[int, str]:
+    node_distances = {}
+    node_connections = {}
+    node_rates = {}
+
+    for line in input_lines(input_path=Path("test_input.txt")):
+        name = re.search(r"(\w{2})(?= has)", line).group()
+        connections = re.findall(r"(\w{2})(?=,|$)", line)
+        rate = re.search(r"(\d+)(?=;)", line).group()
+        node_connections[name] = connections
+        node_rates[name] = rate
+
+    # Calculate distances
+    for root, connections in node_connections.items():
+        unvisited = list(node_connections.keys())
+        distances = {u: 0 if u == root else inf for u in unvisited}
+
+        current = root
+        while True:
+            for neighbour in node_connections[current]:
+                if neighbour in unvisited:
+                    tentative = distances[current] + 1
+                    distances[neighbour] = min(tentative, distances[neighbour])
+
+            unvisited.remove(current)
+            if len(unvisited):
+                min_distance = inf
+                min_key = ""
+                for u in unvisited:
+                    if distances[u] < min_distance:
+                        min_key = u
+                        min_distance = distances[u]
+                if min_distance == inf:
                     break
-                for test_node in connections[subject_node]:
-                    if distances[subject_node] + 1 < distances[test_node]:
-                        distances[test_node] = distances[subject_node] + 1
-                        reached_from[test_node] = subject_node
+                current = min_key
+            else:
+                break
+        node_distances[root] = dict(distances)
 
-            route = [end]
-            while True:
-                next_node = reached_from[route[0]]
-                route.insert(0, next_node)
-                if next_node == start:
-                    break
+    pass
 
-            for i in range(len(route)):
-                for j in range(i + 1, len(route)):
-                    routes[frozenset({route[i], route[j]})] = distances[route[j]] - distances[route[i]]
-
-    assert len(routes) == target_num_routes
-
-    return routes
+def part_2() -> Union[int, str]:
+    ...
 
 
-def main():
-    flow_rates: Dict[str, int] = {}
-    raw_connections: Dict[str, List[str]] = {}
-
-    with open("input.txt", "r") as fh:
-        for line in fh.readlines():
-            res = re.findall(r".* ([A-Z]{2}) .*=(\d*); .* valves? ([A-Z]{2}(, [A-Z]{2})*)", line.strip())[0]
-            flow_rates[res[0]] = int(res[1])
-            raw_connections[res[0]] = res[2].split(", ")
-
-    routes = get_routes(raw_connections)
-
-      
-
-
-
-if __name__ == '__main__':
-    main()
-    """
-    print(
-        get_total_pressure_release(
-            [
-                Node.nodes["DD"],
-                "on",
-                Node.nodes["CC"],
-                Node.nodes["BB"],
-                "on",
-                Node.nodes["AA"],
-                Node.nodes["II"],
-                Node.nodes["JJ"],
-                "on",
-                Node.nodes["II"],
-                Node.nodes["AA"],
-                Node.nodes["DD"],
-                Node.nodes["EE"],
-                Node.nodes["FF"],
-                Node.nodes["GG"],
-                Node.nodes["HH"],
-                "on",
-                Node.nodes["GG"],
-                Node.nodes["FF"],
-                Node.nodes["EE"],
-                "on",
-                Node.nodes["DD"],
-                Node.nodes["CC"],
-                "on",
-                "wait",
-                "wait",
-                "wait",
-                "wait",
-                "wait",
-                "wait",
-            ]
-        )
-    )
-    """
+if __name__ == "__main__":
+    print("Part 1:", part_1())
+    print()
+    print("Part 2:", part_2())
