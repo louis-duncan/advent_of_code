5e3673a794561bba80b5484fc586976a5607eefa
Louis Thurman <louisthurman@gmail.com>
2023-12-11 16:19:22 +0000

    Update day_template.py

--- day_template.py
+++ day_template.py
@@ -18,11 +18,11 @@ def part_2() -> Union[int, str]:
 if __name__ == "__main__":
     part_1_answer = part_1()
     if part_1_answer is not None:
-        print("Part 1:", part_1())
+        print("Part 1:", part_1_answer)
         pyperclip.copy(part_1_answer)
+    print()
 
     part_2_answer = part_2()
     if part_2_answer is not None:
-        print()
-        print("Part 2:", part_2())
+        print("Part 2:", part_2_answer)
         pyperclip.copy(part_2_answer)
