808896ef81f70f288269f6dc1d7667bbfe5e28a2
Louis <louisthurman@gmail.com>
2023-12-17 01:50:18 +0000

    Day 14 2023

--- 2023/day_14/day_14.py
+++ 2023/day_14/day_14.py
@@ -6,19 +6,111 @@ import pyperclip
 https://adventofcode.com/2023/day/14
 """
 
+def roll(grid: AgentCloud, direction: str):
+    if direction == "N":
+        point_list = grid.y_x_sorted
+    elif direction == "E":
+        point_list = grid.x_y_sorted[::-1]
+    elif direction == "S":
+        point_list = grid.y_x_sorted[::-1]
+    else:
+        point_list = grid.x_y_sorted
 
-def part_1() -> Union[int, str]:
-    grid = AgentCloud(input_lines("test_input.txt"))
-
-    for point in grid.points:
+    for point in point_list:
         if point.value != "O":
             continue
 
-        all_above = 
+        if direction == "N":
+            next_points = grid.get_next_in_direction(point.x, point.y, "N")
+
+            if len(next_points) == 0:
+                new_y = 0
+            else:
+                new_y = next_points[-1].y + 1
+
+            distance = point.y - new_y
+        elif direction == "E":
+            next_points = grid.get_next_in_direction(point.x, point.y, "E")
+
+            if len(next_points) == 0:
+                new_x = grid.max_x
+            else:
+                new_x = next_points[0].x - 1
+
+            distance = new_x - point.x
+        elif direction == "S":
+            next_points = grid.get_next_in_direction(point.x, point.y, "S")
+
+            if len(next_points) == 0:
+                new_y = grid.max_y
+            else:
+                new_y = next_points[0].y - 1
+
+            distance = new_y - point.y
+        else:
+            next_points = grid.get_next_in_direction(point.x, point.y, "W")
+
+            if len(next_points) == 0:
+                new_x = 0
+            else:
+                new_x = next_points[-1].x + 1
+
+            distance = point.x - new_x
+
+        if distance > 0:
+            point.move(direction, distance)
+
+
+def part_1() -> Union[int, str]:
+    grid = AgentCloud(input_lines("input.txt"))
+
+    roll(grid, "N")
+
+    load = 0
+    for point in grid.points:
+        if point.value == "O":
+            load += (grid.max_y - point.y) + 1
+
+    return load
 
 
 def part_2() -> Union[int, str]:
-    ...
+    grid = AgentCloud(input_lines("input.txt"))
+
+    directions = "NWSE"
+    states: dict[int, int] = {}  # state_hash: step
+    i = 0
+    caught_loop = False
+    end = 1_000_000_000
+    while i < end:
+        for direction in directions:
+            roll(grid, direction)
+            #print()
+            #print(direction)
+            #print(grid)
+        state = grid.state_hash
+        if caught_loop:
+            pass
+        elif state in states:
+            caught_loop = True
+            i_last_seen = states[state]
+            loop_len = i - i_last_seen
+            iterations_left = end - i
+            num_loops = iterations_left // loop_len
+            i += num_loops * loop_len
+        else:
+            states[state] = i
+        i += 1
+
+        #print(i)
+        #print(grid)
+
+    load = 0
+    for point in grid.points:
+        if point.value == "O":
+            load += (grid.max_y - point.y) + 1
+
+    return load
 
 
 if __name__ == "__main__":
