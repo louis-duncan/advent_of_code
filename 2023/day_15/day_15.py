dc827a83b02aa38041bb8139c3f7730776e0f23f
Louis <louisthurman@gmail.com>
2023-12-17 12:14:24 +0000

    Day 15 2023 - part 1

--- 2023/day_15/day_15.py
+++ 2023/day_15/day_15.py
@@ -1,4 +1,5 @@
 import re
+from dataclasses import dataclass
 from functools import cache
 
 from aoc_utils import *
@@ -26,6 +27,11 @@ def string_hash(string: str) -> int:
         value %= 256
     return value
 
+@dataclass
+class Lense:
+    label: str
+    focal_length: int
+
 
 def part_1() -> Union[int, str]:
     instructions: list[str] = raw_input("input.txt").strip().split(",")
@@ -33,37 +39,54 @@ def part_1() -> Union[int, str]:
 
 
 def part_2() -> Union[int, str]:
-    instructions: list[str] = raw_input("input.txt").strip().split(",")
+    instructions: list[str] = raw_input("test_input.txt").strip().split(",")
 
-    boxes =     
+    boxes: list[list[Lense]] = [[] for _ in range(256)]
 
     for instruction in instructions:
         label, op, value = re.match(r"(\w+)([-=])(\d+)?", instruction).groups()
         if value:
             value = int(value)
 
+        box_i = string_hash(label)
 
-
-
-def find_clash():
-    import random
-
-    alph = [chr(i) for i in range(ord('a'), ord('z') + 1)]
-    results = {}
-    while True:
-        new_str = "".join(random.choices(alph, k=3))
-        new_val = string_hash(new_str)
-        if new_val in results:
-            print(new_str, results[new_val], new_val)
-            break
+        if op == "-":
+            """
+            If the operation character is a dash (-), go to the relevant box and remove the lens with the given label if 
+            it is present in the box. Then, move any remaining lenses as far forward in the box as they can go without 
+            changing their order, filling any space made by removing the indicated lens. (If no lens in that box has the 
+            given label, nothing happens.)
+            """
+            for i in range(len(boxes[box_i])):
+                if boxes[box_i][i].label == label:
+                    boxes[box_i].pop(i)
+                    break
         else:
-            results[new_val] = new_str
+            """
+            If the operation character is an equals sign (=), it will be followed by a number indicating the focal length 
+            of the lens that needs to go into the relevant box; be sure to use the label maker to mark the lens with the 
+            label given in the beginning of the step so you can find it later. There are two possible situations:
+                - If there is already a lens in the box with the same label, replace the old lens with the new lens: remove 
+                  the old lens and put the new lens in its place, not moving any other lenses in the box.
+                - If there is not already a lens in the box with the same label, add the lens to the box immediately behind 
+                  any lenses already in the box. Don't move any of the other lenses when you do this. If there aren't any 
+                  lenses in the box, the new lens goes all the way to the front of the box.
+            """
+            for i in range(len(boxes[box_i])):
+                if boxes[box_i][i].label == label:
+                    boxes[box_i][i] = Lense(label, value)
+                    break
+            else:
+                boxes[box_i].append(Lense(label, value))
+
+    total = 0
+    for box in boxes:
+        for lense in box:
+            total += lense.focal_length
+    return total
 
 
 if __name__ == "__main__":
-    find_clash()
-    exit()
-
     part_1_answer = part_1()
     if part_1_answer is not None:
         print("Part 1:", part_1_answer)
