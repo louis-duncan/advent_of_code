228d16b14f592df51a4606fbb514e5f8e9746dfa
Louis <louisthurman@gmail.com>
2023-12-26 15:26:09 +0000

    Update template

--- day_template.py
+++ day_template.py
@@ -1,6 +1,9 @@
-from aoc_utils import *
+import time
 
 import pyperclip
+
+from aoc_utils import *
+
 
 """
 {DAY_LINK}
@@ -16,13 +19,17 @@ def part_2() -> Union[int, str]:
 
 
 if __name__ == "__main__":
+    p1_start = time.time()
     part_1_answer = part_1()
+    p1_duration = time.time() - p1_start
     if part_1_answer is not None:
-        print("Part 1:", part_1_answer)
+        print(f"Part 1 ({p1_duration * 1000:.2f}ms):", part_1_answer)
         pyperclip.copy(part_1_answer)
     print()
 
+    p2_start = time.time()
     part_2_answer = part_2()
+    p2_duration = time.time() - p2_start
     if part_2_answer is not None:
-        print("Part 2:", part_2_answer)
+        print(f"Part 2 ({p2_duration * 1000:.2f}ms):", part_2_answer)
         pyperclip.copy(part_2_answer)
