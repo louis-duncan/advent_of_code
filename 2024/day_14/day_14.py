2e4f12185b9fbe04d51795096ae1b2f1ee583c5a
Louis <louisthurman@gmail.com>
2024-12-21 12:40:02 +0000

    2024-14

--- 2024/day_14/day_14.py
+++ 2024/day_14/day_14.py
@@ -1,3 +1,4 @@
+import math
 import time
 import re
 
@@ -10,22 +11,96 @@ from aoc_utils import *
 https://adventofcode.com/2024/day/14
 """
 
-_INPUT_PATH = INPUT_PATH_TEST
+_INPUT_PATH = INPUT_PATH  # _TEST
 
 
 def part_1() -> Union[int, str]:
-    grid = AgentGrid()
+    width = 101
+    height = 103
+    grid = AgentGrid(None)
+    robot: VelocityPoint
     for line in input_lines(_INPUT_PATH):
         nums = [int(n) for n in re.findall(r"-?\d+", line)]
-        agent = PointAgent(
+        robot = VelocityPoint(
             "#",
             x=nums[0], y=nums[1],
+            vx=nums[2], vy=nums[3]
         )
+        grid.add(robot)
 
+    for _ in range(100):
+        for robot in grid.points:
+            robot.update()
+            out_of_bounds = False
+            new_x, new_y = robot.x_y
+            if new_x >= width or new_x < 0:
+                out_of_bounds = True
+                new_x = new_x % width
+            if new_y >= height or new_y < 0:
+                out_of_bounds = True
+                new_y = new_y % height
+            if out_of_bounds:
+                robot.move_to(new_x, new_y)
+
+    quad_one = grid.get_region(
+        (0, 0), ((width // 2) - 1, (height // 2) - 1)
+    )
+    quad_two = grid.get_region(
+        ((width // 2) + 1, 0), (width - 1, (height // 2) - 1)
+    )
+    quad_three = grid.get_region(
+        (0, (height // 2) + 1), ((width // 2) - 1, height - 1),
+    )
+    quad_four = grid.get_region(
+        ((width // 2) + 1, (height // 2) + 1), (width - 1, height - 1),
+    )
+
+    return len(quad_one) * len(quad_two) * len(quad_three) * len(quad_four)
 
 
 def part_2() -> Union[int, str]:
-    ...
+    width = 101
+    height = 103
+    grid = AgentGrid(None)
+    robot: VelocityPoint
+    for line in input_lines(_INPUT_PATH):
+        nums = [int(n) for n in re.findall(r"-?\d+", line)]
+        robot = VelocityPoint(
+            "#",
+            x=nums[0], y=nums[1],
+            vx=nums[2], vy=nums[3]
+        )
+        grid.add(robot)
+
+    n = 0
+    occupied_points = set()
+    num_robots = len(grid.points)
+    while True:
+        occupied_points.clear()
+        for robot in grid.points:
+            robot.update()
+            out_of_bounds = False
+            new_x, new_y = robot.x_y
+            if new_x >= width or new_x < 0:
+                out_of_bounds = True
+                new_x = new_x % width
+            if new_y >= height or new_y < 0:
+                out_of_bounds = True
+                new_y = new_y % height
+            if out_of_bounds:
+                robot.move_to(new_x, new_y)
+            occupied_points.add((new_x, new_y))
+        n += 1
+
+        if len(occupied_points) == num_robots:
+            print(str(grid))
+            print(n)
+            try:
+                input()
+            except KeyboardInterrupt:
+                break
+
+    return n
 
 
 if __name__ == "__main__":
